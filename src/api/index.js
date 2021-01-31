fa18bf608fc0decf26bc69d2ab2d41534c7e55aa
Basil Satti <baselka76@gmail.com>
2021-01-31 19:36:02 +0800

    api for build changed

--- src/api/index.js
+++ src/api/index.js
@@ -5,7 +5,7 @@ let API_ROOT
 let headers
 
 if(process.env.NODE_ENV === 'production') {
-    API_ROOT = `http://localhost:3000/api/v1/back-office`
+    API_ROOT = `https://api-staging.happy-season.com/api/v1/back-office`
 } else {
     API_ROOT = `https://api-staging.happy-season.com/api/v1/back-office`
 }
