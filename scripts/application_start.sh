7a48f8f963913f6b700a308e8afb32d80ddf9ed7
baselka <baselka76@gmail.com>
2021-07-26 02:41:11 +0800

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 #give permission for everything in the express-app directory
 sudo chmod -R 777 /var/www/staging.happy-season.com/html/HS-Back-office
 
