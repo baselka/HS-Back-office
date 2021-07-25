43020c95a847f1b7a55e8fdd49e42cd0301485fc
baselka <baselka76@gmail.com>
2021-07-26 01:31:54 +0800

    install pm2

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+npm install pm2 -g
 #create our working directory if it doesnt exist
 DIR="/var/www/staging.happy-season.com/html/HS-Back-office"
 if [ -d "$DIR" ]; then
